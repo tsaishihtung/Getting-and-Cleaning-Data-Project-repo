b02f495c4ccca7b5357880ab25cd8a21a1bfd454
Tsai-Shih Tung <tsaishihtung@gmail.com>
2015-01-26 08:17:07 +0800

    Code v3

--- run_analysis.R
+++ run_analysis.R
@@ -80,29 +80,10 @@ names(df_new)<-gsub("-",replacement=".",names(df_new))
 #Step4 is finished.
 
 #Step5:
-#
+#Use group_by function to group subject and activity.
+#Use summarise_each function to calculate each mean of the variables.
 df_new1<-group_by(df_new,subject,activity)
-names(df_new1)[3:68]<-names(Xtest)[1:66]
-df_mean<-summarize(df_new1,mean1=mean(V1),mean2=mean(V2),mean3=mean(V3),
- mean4=mean(V4),mean5=mean(V5),mean6=mean(V6),mean7=mean(V7),
- mean8=mean(V8),mean9=mean(V9),mean10=mean(V10),
- mean11=mean(V11),mean12=mean(V12),mean13=mean(V13),
- mean14=mean(V14),mean15=mean(V15),mean16=mean(V16),mean17=mean(V17),
- mean18=mean(V18),mean19=mean(V19),mean20=mean(V20),
- mean21=mean(V21),mean22=mean(V22),mean23=mean(V23),
- mean24=mean(V24),mean25=mean(V25),mean26=mean(V26),mean27=mean(V27),
- mean28=mean(V28),mean29=mean(V29),mean30=mean(V30),
- mean31=mean(V31),mean32=mean(V32),mean33=mean(V33),
- mean34=mean(V34),mean35=mean(V35),mean36=mean(V36),mean37=mean(V37),
- mean38=mean(V38),mean39=mean(V39),mean40=mean(V40),
- mean41=mean(V41),mean42=mean(V42),mean43=mean(V43),
- mean44=mean(V44),mean45=mean(V45),mean46=mean(V46),mean47=mean(V47),
- mean48=mean(V48),mean49=mean(V49),mean50=mean(V50),
- mean51=mean(V51),mean52=mean(V52),mean53=mean(V53),
- mean54=mean(V54),mean55=mean(V55),mean56=mean(V56),mean57=mean(V57),
- mean58=mean(V58),mean59=mean(V59),mean60=mean(V60),
- mean61=mean(V61),mean62=mean(V62),mean63=mean(V63),
- mean64=mean(V64),mean65=mean(V65),mean66=mean(V66))
+df_mean<-summarise_each(df_new1,funs(mean))
 
 write.table(df_mean,file="./data/tidy_data_set.txt",row.name=FALSE)
 #Step5 is finished.
